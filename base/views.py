bced90825c04f4ed55b6aaaae62ec67696ffc44f
Alavi Khan <leonasting@gmail.com>
2023-01-13 00:13:14 -0800

    template

--- base/views.py
+++ base/views.py
@@ -3,8 +3,22 @@ from django.http import HttpResponse
  
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Home Page")
+rooms = [
+        {"id":1, "name":'Lets Learn Python!'},
+        {"id":2, "name":'Design with Me!'},
+        {"id":3, "name":'Front End Developers '},
+        ]
 
-def room(request):
-    return HttpResponse("Room Page")
+
+def home(request):
+    context = {'rooms' : rooms}
+    return render(request,'base/home.html',context) # Passing dictionary key value pair
+
+def room(request,pk):# Pk is acting like query for database query
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context={'room':room}
+
+    return render(request,'base/room.html',context) 
