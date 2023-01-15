2e61637e8aabfe0c695ffd8eee9f0b3517c60424
Alavi Khan <leonasting@gmail.com>
2023-01-14 18:59:18 -0800

    Till DB part
    Admin, 2 migration , Views, Models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length = 200)
+    def __str__(self): # String Representation
+        return self.name
+
+class Room(models.Model):
+    host =  models.ForeignKey(User,on_delete = models.SET_NULL,null=True)  
+    topic = models.ForeignKey(Topic,on_delete = models.SET_NULL,null=True)# allowing empty values in DB
+    name = models.CharField(max_length=200)
+    description = models.TextField(null = True,blank = True) # Null is for databse having empty, blacnk is for form field
+    #participants =  # If you comment on something
+    updated = models.DateTimeField(auto_now=True) # Latest Interaction captured
+    created = models.DateTimeField(auto_now_add=True) # First Interaction captured
+
+    def __str__(self): # String Representation
+        return self.name
+
+
+class Message(models.Model):# Many to 1 with Room
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    room = models.ForeignKey(Room,on_delete = models.CASCADE)# if ROOM is deleted message is also deleted
+    body = models.TextField() 
+    updated = models.DateTimeField(auto_now=True) # Latest Interaction captured
+    created = models.DateTimeField(auto_now_add=True) # First Interaction captured
+
+    def __str__(self): # TO represent only the first 50 characters
+        return self.body[:50]
+
+
--- base/views.py
+++ base/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
- 
+from .models import Room  # Model we want to query
 # Create your views here.
+
+
 
 rooms = [
         {"id":1, "name":'Lets Learn Python!'},
         {"id":2, "name":'Design with Me!'},
         {"id":3, "name":'Front End Developers '},
         ]
-
+ 
 
 def home(request):
+    rooms = Room.objects.all() # Overwrites the earlier defination of rooms
     context = {'rooms' : rooms}
     return render(request,'base/home.html',context) # Passing dictionary key value pair
 
 def room(request,pk):# Pk is acting like query for database query
     room = None
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
+    #for i in rooms:
+    #    if i["id"] == int(pk):
+    #        room = i
+    room = Room.objects.get(id=pk)
     context={'room':room}
 
     return render(request,'base/room.html',context) 
